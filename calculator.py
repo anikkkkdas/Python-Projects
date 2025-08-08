b7c41558864ca5b40f961565e8dbbae181394b23
Anik Das <anikdas0010@gmail.com>
2025-08-08 08:42:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+try:
+    a = int(input("Enter the First Number: "))
+    b = int(input("Enter the Second Number: "))
+
+    print("What kind of operation you want to perform. \n Press + for addition \n Press - for substraction \n Press / for division \n Press * for multiplication")
+
+    o = input("Enter Operation: ")
+    match o:
+        case "+":
+            print(f"The Result is {a+b}" )
+        case "-":
+            print(f"The Result is {a-b}" )
+        case "*":
+            print(f"The Result is {a*b}" )
+        case "/":
+            print(f"The Result is {a/b}" )
+        case default:
+            print("There was an Error")
+except Exception as e:
+    print("Invalid input. Please enter numbers only.")
+    print(f"Error details: {e}")
+
